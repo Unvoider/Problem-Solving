635e1c7bb506207208b8a3ada15dfda0820c9b96
Unvoider <dowon4321@gmail.com>
2026-02-11 13:51:46 +0900

    refactor(js): change pointer logic from empty ascending to full ascending (BOJ 2164)

--- Baekjoon/Class2/26.2164 카드2.js
+++ Baekjoon/Class2/26.2164 카드2.js
@@ -4,14 +4,14 @@ const input = require("fs").readFileSync(inputFile, "utf8").trim();
 const n = Number(input);
 const cards = Array.from({ length: n }, (_, i) => i + 1); // queue full 기능이 없는 원형 큐
 let front = 0; // full ascending
-let rear = 0; // empty ascending
+let rear = -1; // full ascending
 const forward = (idx) => (idx + 1) % n;
 
 let next;
-while((next = forward(front)) !== rear) { // 1개 남으면 종료
-    front = next; // 맨 위 제거
-    cards[rear] = cards[front]; // 맨 위 -> 맨 아래
+while(front !== rear) { // 1개 남으면 종료
+    front = forward(front); // 맨 위 제거
+    rear = forward(rear); // 맨 위 -> 맨 아래
+    cards[rear] = cards[front];
     front = forward(front);
-    rear = forward(rear);
 }
 console.log(cards[front]);
