558ea1a15e1e685d12bf6c4b0bbf733ea0d650eb
Unvoider <dowon4321@gmail.com>
2026-02-19 18:02:08 +0900

    refactor(java): nest Sieve class into Main class (BOJ 1978, 1929)

--- Baekjoon/Class2/37.1929 소수 구하기.java
+++ Baekjoon/Class2/37.1929 소수 구하기.java
@@ -3,25 +3,25 @@ import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-class Sieve {
-    private boolean[] isPrime;
-
-    public Sieve(int max) {
-        isPrime = new boolean[max + 1];
-        Arrays.fill(isPrime, true);
-        isPrime[0] = isPrime[1] = false;
-        for(int i = 2; i * i <= max; i++) // 2부터 sqrt(max)까지 소수 찾기
-            if(isPrime[i])
-                for(int j = i * i; j <= max; j += i) // 소수면 제곱부터 배수를 지우기
-                    isPrime[j] = false;
-    }
-
-    public boolean isPrime(int num) {
-        return isPrime[num];
-    }
-}
-
 class Main {
+    private static class Sieve {
+        private boolean[] isPrime;
+
+        public Sieve(int max) {
+            isPrime = new boolean[max + 1];
+            Arrays.fill(isPrime, true);
+            isPrime[0] = isPrime[1] = false;
+            for(int i = 2; i * i <= max; i++) // 2부터 sqrt(max)까지 소수 찾기
+                if(isPrime[i])
+                    for(int j = i * i; j <= max; j += i) // 소수면 제곱부터 배수를 지우기
+                        isPrime[j] = false;
+        }
+
+        public boolean isPrime(int num) {
+            return isPrime[num];
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         int n, m;
         Sieve sieve;
