4bf309077a35669e3da99723c2a93af28f489516
Unvoider <dowon4321@gmail.com>
2026-03-28 23:40:04 +0900

    refactor: remove redundant comparison (BOJ 11053)

--- Baekjoon/Class4/04.11053 가장 긴 증가하는 부분 수열.cpp
+++ Baekjoon/Class4/04.11053 가장 긴 증가하는 부분 수열.cpp
@@ -12,16 +12,13 @@ int main() {
 
     vector<int> a(n);
     vector<int> max_lengths(n, 1);
-    int max_length = 1;
     for (int i = 0; i < n; i++) {
         cin >> a[i];
         for (int j = 0; j < i; j++) // 각 도착점의 최대 길이 찾기
             if (a[i] > a[j])
                 max_lengths[i] = max(max_lengths[i], max_lengths[j] + 1);
-        if (max_lengths[i] > max_length) // 전체 도착점 중 최대 길이 찾기
-            max_length = max(max_length, max_lengths[i]);
     }
 
-    cout << max_length;
+    cout << *max_element(max_lengths.begin(), max_lengths.end());
     return 0;
 }
--- Baekjoon/Class4/04.11053 가장 긴 증가하는 부분 수열.py
+++ Baekjoon/Class4/04.11053 가장 긴 증가하는 부분 수열.py
@@ -3,14 +3,11 @@ input = sys.stdin.readline
 
 n = int(input())
 a = list(map(int, input().split()))
-maxLengths = [1] * n
-maxLength = 1
+max_lengths = [1] * n
 
 for i in range(n):
     for j in range(i): #각 도착점의 최대 길이 찾기
         if a[i] > a[j]:
-            maxLengths[i] = max(maxLengths[i], maxLengths[j] + 1)
-    if maxLengths[i] > maxLength: #전체 도착점 중 최대 길이 찾기
-        maxLength = max(maxLength, maxLengths[i])
+            max_lengths[i] = max(max_lengths[i], max_lengths[j] + 1)
 
-print(maxLength)
+print(max(max_lengths))
